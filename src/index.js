33a993ddf647ebe45ac7fb2a649db0ac4971069a
DebjitPramanick <dpramanick9990@gmail.com>
2020-12-03 20:04:54 +0530

    Version 1

--- src/index.js
+++ src/index.js
@@ -1,17 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//Import CSS file
+
+import './index.css'
+
+function Booklist(){
+  return (
+  <section className="booklist">
+    <Book/>
+    <Book/>
+    <Book/>
+    <Book/>
+    <Book/>
+    <Book/>
+    <Book/>
+    <Book/>
+  </section>
+  );
+}
+
+const Book = () =>{
+  return(
+    <article className="bookcards">
+      <BookImage/>
+      <Title/>
+      <Author/>
+      <Price/>
+    </article>
+  )
+}
+
+const BookImage = () =>{return <div className="bookimage"><img  src="https://images-eu.ssl-images-amazon.com/images/I/31970VMFv5L._AC_SX184_.jpg" alt=""/></div>}
+const Title = () =>{return <a className="bookname" href="#">Eating in the Age of Dieting: A collection of notes and essays from over the years</a>}
+const Author =() =>{return <p className="bookauthor" >Paperback</p>}
+const Price = () =>{return <p className="bookprice" >$180.00</p>}
+
+ReactDOM.render(<Booklist/>,document.getElementById('root'));
