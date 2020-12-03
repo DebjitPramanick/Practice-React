e357dabda16fd1c02163e9636dbe570e264022f4
DebjitPramanick <dpramanick9990@gmail.com>
2020-12-04 00:19:15 +0530

    Version 1

--- src/index.js
+++ src/index.js
@@ -6,35 +6,231 @@ import ReactDOM from 'react-dom';
 
 import './index.css'
 
+//Set up vars
+
+const books = [
+  {
+    img: "https://images-eu.ssl-images-amazon.com/images/I/31970VMFv5L._AC_SX184_.jpg",
+    title: "Eating in the Age of Dieting: A collection of notes and essays from over the years",
+    author: "Rujuta Diwekar",
+    price: "₹321.00"
+  }
+  ,
+  {
+    img: "https://images-eu.ssl-images-amazon.com/images/I/41gBLwftOSL._AC_SX184_.jpg",
+    title: "Shuggie Bain: Winner of Booker Prize 2020",
+    author: "Douglas Stuart",
+    price: "₹316.00"
+  }
+  ,
+  {
+    img: "https://images-na.ssl-images-amazon.com/images/I/41dElS3Q0mL._SX324_BO1,204,203,200_.jpg",
+    title: "The Archer",
+    author: "Paulo Coelho",
+    price: "₹145.00"
+  }
+  ,
+  {
+    img: "https://images-na.ssl-images-amazon.com/images/I/41a+W3AFhyL._SX360_BO1,204,203,200_.jpg",
+    title: "Portraits of Power: Half a Century of Being at Ringside",
+    author: "N.K. Singh",
+    price: "₹396.00"
+  }
+  ,
+  {
+    img: "https://images-na.ssl-images-amazon.com/images/I/51ExYzLZs1L._SX418_BO1,204,203,200_.jpg",
+    title: "Harry Potter and the Prisoner of Azkaban ",
+    author: "J.K. Rowling",
+    price: "₹690.00"
+  }
+  ,
+  {
+    img: "https://images-na.ssl-images-amazon.com/images/I/51ka6kZlAIL._SX322_BO1,204,203,200_.jpg",
+    title: "Why Not Me? A Feeling of Millions ",
+    author: "Anubhav Agrawal",
+    price: "₹159.00"
+  }
+  ,
+  {
+    img: "https://images-eu.ssl-images-amazon.com/images/I/31970VMFv5L._AC_SX184_.jpg",
+    title: "The Hitman: The Rohit Sharma Story ",
+    author: "Vijay Lokapally",
+    price: "₹374.00"
+  }
+  ,
+  {
+    img: "https://images-eu.ssl-images-amazon.com/images/I/31970VMFv5L._AC_SX184_.jpg",
+    title: "Eating in the Age of Dieting: A collection of notes and essays from over the years",
+    author: "Rujuta Diwekar",
+    price: "₹321.00"
+  }
+  ,
+  {
+    img: "https://images-eu.ssl-images-amazon.com/images/I/41gBLwftOSL._AC_SX184_.jpg",
+    title: "Shuggie Bain: Winner of Booker Prize 2020",
+    author: "Douglas Stuart",
+    price: "₹316.00"
+  }
+  ,
+  {
+    img: "https://images-na.ssl-images-amazon.com/images/I/41dElS3Q0mL._SX324_BO1,204,203,200_.jpg",
+    title: "The Archer",
+    author: "Paulo Coelho",
+    price: "₹145.00"
+  }
+  ,
+  {
+    img: "https://images-na.ssl-images-amazon.com/images/I/41a+W3AFhyL._SX360_BO1,204,203,200_.jpg",
+    title: "Portraits of Power: Half a Century of Being at Ringside",
+    author: "N.K. Singh",
+    price: "₹396.00"
+  }
+  ,
+  {
+    img: "https://images-na.ssl-images-amazon.com/images/I/51ExYzLZs1L._SX418_BO1,204,203,200_.jpg",
+    title: "Harry Potter and the Prisoner of Azkaban ",
+    author: "J.K. Rowling",
+    price: "₹690.00"
+  }
+  ,
+  {
+    img: "https://images-na.ssl-images-amazon.com/images/I/51ka6kZlAIL._SX322_BO1,204,203,200_.jpg",
+    title: "Why Not Me? A Feeling of Millions ",
+    author: "Anubhav Agrawal",
+    price: "₹159.00"
+  }
+  ,
+  {
+    img: "https://images-eu.ssl-images-amazon.com/images/I/31970VMFv5L._AC_SX184_.jpg",
+    title: "The Hitman: The Rohit Sharma Story ",
+    author: "Vijay Lokapally",
+    price: "₹374.00"
+  }
+  ,
+  {
+    img: "https://images-eu.ssl-images-amazon.com/images/I/31970VMFv5L._AC_SX184_.jpg",
+    title: "Eating in the Age of Dieting: A collection of notes and essays from over the years",
+    author: "Rujuta Diwekar",
+    price: "₹321.00"
+  }
+  ,
+  {
+    img: "https://images-eu.ssl-images-amazon.com/images/I/41gBLwftOSL._AC_SX184_.jpg",
+    title: "Shuggie Bain: Winner of Booker Prize 2020",
+    author: "Douglas Stuart",
+    price: "₹316.00"
+  }
+  ,
+  {
+    img: "https://images-na.ssl-images-amazon.com/images/I/41dElS3Q0mL._SX324_BO1,204,203,200_.jpg",
+    title: "The Archer",
+    author: "Paulo Coelho",
+    price: "₹145.00"
+  }
+  ,
+  {
+    img: "https://images-na.ssl-images-amazon.com/images/I/41a+W3AFhyL._SX360_BO1,204,203,200_.jpg",
+    title: "Portraits of Power: Half a Century of Being at Ringside",
+    author: "N.K. Singh",
+    price: "₹396.00"
+  }
+  ,
+  {
+    img: "https://images-na.ssl-images-amazon.com/images/I/51ExYzLZs1L._SX418_BO1,204,203,200_.jpg",
+    title: "Harry Potter and the Prisoner of Azkaban ",
+    author: "J.K. Rowling",
+    price: "₹690.00"
+  }
+  ,
+  {
+    img: "https://images-na.ssl-images-amazon.com/images/I/51ka6kZlAIL._SX322_BO1,204,203,200_.jpg",
+    title: "Why Not Me? A Feeling of Millions ",
+    author: "Anubhav Agrawal",
+    price: "₹159.00"
+  }
+  ,
+  {
+    img: "https://images-eu.ssl-images-amazon.com/images/I/31970VMFv5L._AC_SX184_.jpg",
+    title: "The Hitman: The Rohit Sharma Story ",
+    author: "Vijay Lokapally",
+    price: "₹374.00"
+  }
+  ,
+  {
+    img: "https://images-eu.ssl-images-amazon.com/images/I/31970VMFv5L._AC_SX184_.jpg",
+    title: "Eating in the Age of Dieting: A collection of notes and essays from over the years",
+    author: "Rujuta Diwekar",
+    price: "₹321.00"
+  }
+  ,
+  {
+    img: "https://images-eu.ssl-images-amazon.com/images/I/41gBLwftOSL._AC_SX184_.jpg",
+    title: "Shuggie Bain: Winner of Booker Prize 2020",
+    author: "Douglas Stuart",
+    price: "₹316.00"
+  }
+  ,
+  {
+    img: "https://images-na.ssl-images-amazon.com/images/I/41dElS3Q0mL._SX324_BO1,204,203,200_.jpg",
+    title: "The Archer",
+    author: "Paulo Coelho",
+    price: "₹145.00"
+  }
+  ,
+  {
+    img: "https://images-na.ssl-images-amazon.com/images/I/41a+W3AFhyL._SX360_BO1,204,203,200_.jpg",
+    title: "Portraits of Power: Half a Century of Being at Ringside",
+    author: "N.K. Singh",
+    price: "₹396.00"
+  }
+  ,
+  {
+    img: "https://images-na.ssl-images-amazon.com/images/I/51ExYzLZs1L._SX418_BO1,204,203,200_.jpg",
+    title: "Harry Potter and the Prisoner of Azkaban ",
+    author: "J.K. Rowling",
+    price: "₹690.00"
+  }
+  ,
+  {
+    img: "https://images-na.ssl-images-amazon.com/images/I/51ka6kZlAIL._SX322_BO1,204,203,200_.jpg",
+    title: "Why Not Me? A Feeling of Millions ",
+    author: "Anubhav Agrawal",
+    price: "₹159.00"
+  }
+  ,
+  {
+    img: "https://images-eu.ssl-images-amazon.com/images/I/31970VMFv5L._AC_SX184_.jpg",
+    title: "The Hitman: The Rohit Sharma Story ",
+    author: "Vijay Lokapally",
+    price: "₹374.00"
+  }
+  ,
+  
+  
+]
+
+
 function Booklist(){
   return (
   <section className="booklist">
-    <Book/>
-    <Book/>
-    <Book/>
-    <Book/>
-    <Book/>
-    <Book/>
-    <Book/>
-    <Book/>
+    {books.map((book) => {
+      return <Book book={book}/>
+    })}
   </section>
   );
 }
 
-const Book = () =>{
+const Book = (props) =>{
   return(
     <article className="bookcards">
-      <BookImage/>
-      <Title/>
-      <Author/>
-      <Price/>
+      <div className="bookimage"><img  src={props.book.img} alt=""/></div>
+      <a className="bookname" href="#">{props.book.title}</a>
+      <p className="bookauthor" >{props.book.author}</p>
+      <p className="bookprice" >{props.book.price}</p>
+      <button className="buybtn" >Buy</button>
     </article>
   )
 }
 
-const BookImage = () =>{return <div className="bookimage"><img  src="https://images-eu.ssl-images-amazon.com/images/I/31970VMFv5L._AC_SX184_.jpg" alt=""/></div>}
-const Title = () =>{return <a className="bookname" href="#">Eating in the Age of Dieting: A collection of notes and essays from over the years</a>}
-const Author =() =>{return <p className="bookauthor" >Paperback</p>}
-const Price = () =>{return <p className="bookprice" >$180.00</p>}
 
 ReactDOM.render(<Booklist/>,document.getElementById('root'));
