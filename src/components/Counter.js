e1220abc6011d0ecc2c47c5c96f17756f9ef01df
DebjitPramanick <dpramanick9990@gmail.com>
2021-01-12 15:18:22 +0530

    Version 1

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Counter = () => {
+
+    const [count, setCount] = useState(0);
+
+    const increase = (e) =>{
+        e.preventDefault();
+        setCount(count + 1);
+    }
+
+    const decrease = (e) =>{
+        e.preventDefault();
+        setCount(count - 1);
+    }
+
     return (
         <div className="container">
-            This is a counter.
+            <h1>{count}</h1>
+            <div className="btn-container">
+                <button className="btn" onClick={decrease}>-1</button>
+                <button className="btn" onClick={increase}>+1</button>
+            </div>
         </div>
     )
 }
