08deb5dfba186b860872b4f784cafac99667984d
DebjitPramanick <dpramanick9990@gmail.com>
2021-01-13 02:26:13 +0530

    Version 1

--- src/components/Drag&Drop/DragAndDrop.js
+++ src/components/Drag&Drop/DragAndDrop.js
@@ -14,7 +14,17 @@ const DragAndDrop = () => {
 
                     <Cards id="card-3" className="draggable-card"
                     draggable="true">
-                        <p>Card 1</p>
+                        <p>Card 3</p>
+                    </Cards>
+
+                    <Cards id="card-5" className="draggable-card"
+                    draggable="true">
+                        <p>Card 5</p>
+                    </Cards>
+
+                    <Cards id="card-7" className="draggable-card"
+                    draggable="true">
+                        <p>Card 7</p>
                     </Cards>
                 </Board>
 
@@ -22,6 +32,16 @@ const DragAndDrop = () => {
                     <Cards id="card-2" className="draggable-card"
                     draggable="true">
                         <p>Card 2</p>
+                    </Cards>
+
+                    <Cards id="card-4" className="draggable-card"
+                    draggable="true">
+                        <p>Card 4</p>
+                    </Cards>
+
+                    <Cards id="card-6" className="draggable-card"
+                    draggable="true">
+                        <p>Card 6</p>
                     </Cards>
                 </Board>
             </div>
---
+++ src/components/Redux/Store.js
@@ -0,0 +1,36 @@
+import redux from 'redux'
+import reduxLogger from 'redux-logger'
+import { buyPen } from './Actions';
+import { penReducer, pencilReducer } from "./Reducer"
+
+const createStore = redux.createStore;
+const combineReducers = redux.combineReducers;
+const applyMiddleWare = redux.applyMiddleware;
+const logger = reduxLogger.createLogger();
+
+//Combine multiple reducers
+
+const rootReducer = combineReducers({
+    pen: penReducer,
+    pencil: pencilReducer
+})
+
+//Create store with reducer
+
+const store = createStore(rootReducer, applyMiddleWare(logger));
+console.log('Initial state', store.getState());
+
+
+//Subscribe and unsubscribe to store
+//Unscribe is the return value of subscribe method
+
+const unsubscribe = store.subscribe(() => {
+    console.log('Updated State',store.getState());
+})
+
+store.dispatch(buyPen);
+store.dispatch(buyPen);
+store.dispatch(buyPen);
+
+
+unsubscribe();
