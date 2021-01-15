c9d44b8ed89235e152d57066f6ba382272ed5a8a
DebjitPramanick <dpramanick9990@gmail.com>
2021-01-15 20:51:55 +0530

    Version 1

--- src/components/UploadImage.js
+++ src/components/UploadImage.js
@@ -1,25 +1,21 @@
 import React, { useState,useRef } from 'react'
 import AddPhotoAlternateIcon from '@material-ui/icons/AddPhotoAlternate';
+import ReactCrop from 'react-image-crop';
+import 'react-image-crop/dist/ReactCrop.css';
 
 const UploadImage = () => {
 
-    const [img, setImg] = useState('https://us.123rf.com/450wm/mialima/mialima1603/mialima160300025/55096766-stock-vector-male-user-icon-isolated-on-a-white-background-account-avatar-for-web-user-profile-picture-unknown-ma.jpg?ver=6');
-
+    const [src, setSrc] = useState('https://us.123rf.com/450wm/mialima/mialima1603/mialima160300025/55096766-stock-vector-male-user-icon-isolated-on-a-white-background-account-avatar-for-web-user-profile-picture-unknown-ma.jpg?ver=6');
+    const [image, setImage] = useState()
 
     const fileInput = useRef(null)
 
     const displayChange = e => {
-        const reader = new FileReader();
-        reader.onload = () =>{
-            if(reader.readyState === 2){
-                setImg(reader.result);
-            }
-            else{
-                alert("Error occured.")
-            }
-        }
+        setImg(URL.createObjectURL(e.target.files[0]));
+    }
 
-        reader.readAsDataURL(e.target.files[0]);
+    onload = (image) => {
+
     }
 
 
@@ -35,6 +31,12 @@ const UploadImage = () => {
             <input type="file" className="choose-file"
             accept="image/*" onChange={(e) => displayChange(e)}
             ref={fileInput}></input>
+
+            <div className="crop-field">
+                {src && (
+                    <ReactCrop onImageLoaded={onload}/>
+                )} 
+            </div>
 
             <button className="btn3"
             onClick={()=>fileInput.current.click()}>
